57def5c1d7d7fa349bb234627217f7e76d4e7849
Efennn1 <122887393+Efennn1@users.noreply.github.com>
2023-02-01 23:21:56 +0300

    Add files via upload

---
+++ lab1_1.js
@@ -0,0 +1,9 @@
+"use strict";
+const x1 = 3;
+const y1 = 3;
+const x2 = 8;
+const y2 = 8;
+const name1 = math.abs(x2 - x1);
+const name2 = math.abs(y2 - y1);
+const square = name1 * name2;
+console.log("Площадь прямоуголиника: ", square);
