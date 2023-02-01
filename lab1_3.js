6a151b7f7ec0deffa51430909f3591fb2a1e32c8
Efennn1 <122887393+Efennn1@users.noreply.github.com>
2023-02-01 23:19:25 +0300

    Add files via upload

---
+++ lab1_3.js
@@ -0,0 +1,8 @@
+"use strict";
+const n = 0;
+const m = 100;
+const m1 = m - n;  
+const zcx = Math.floor(Math.random() * m1) + n;
+const c = Math.floor(zcx / 2);
+const p = c * 2 + 1;
+console.log(p);
